e27255fe01b8c00c588ac7681a90c3321d840229
ProstoGhost <tam_1910@bk.ru>
2024-03-29 09:07:34 +0500

    fixed canvas raid and added beta support for gyroscope on Android

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
-document.addEventListener('mousemove', e => {
-    Object.assign(document.documentElement,{
-        style: `
-            --move-x: ${(e.clientX - window.innerWidth / 2)* -.005}deg;
-            --move-y: ${(e.clientY - window.innerHeight / 2 )* -.01}deg;
-        `
-    })
-})
+window.addEventListener('deviceorientation', function(event) {
+    if (event.alpha !== null && event.beta !== null && event.gamma !== null) {
+        console.log('Device has a gyroscope.');
+        Object.assign(document.documentElement, {
+            style: `
+                --move-x: 0deg;
+                --move-y: ${(event.beta - 90)/ -15}deg;
+            `
+        })
+    } else {
+        console.log('Device does not have a gyroscope.');
+        window.addEventListener('mousemove', e => {
+            Object.assign(document.documentElement,{
+                style: `
+                    --move-x: ${(e.clientX - window.innerWidth / 2)* -.005}deg;
+                    --move-y: ${(e.clientY - window.innerHeight / 2 )* .012}deg;
+                `
+            })
+        });
+    }
+});
